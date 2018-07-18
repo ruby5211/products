dc10174b5c3bf888d25d203a66af4848e713800d
Ruby Shih <ruby923@gmail.com>
2018-07-18 21:05:13 +0800

    add file to computer

--- product.py
+++ product.py
@@ -11,3 +11,8 @@ print(products)
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')  # \n 換行 ＃ write 寫入
+
+
