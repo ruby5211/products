dade6c7182055884a1e53e07c3d34cb7d7b2313c
Ruby Shih <ruby923@gmail.com>
2018-07-18 20:47:09 +0800

    add product

---
+++ product.py
@@ -0,0 +1,12 @@
+products = []
+while True:
+    name = input('請輸入商品名稱：')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格：')
+    p = [name, price]
+    products.append(p) #小清單裝入大清單 （二維清單）
+print(products)
+
+products[0][0]
+
