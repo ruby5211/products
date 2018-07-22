ecf410853358b7d62611414bcec49d6a79ab02be
Ruby Shih <ruby923@gmail.com>
2018-07-22 23:05:42 +0800

    final version

--- product.py
+++ product.py
@@ -1,12 +1,19 @@
+import os # os = operating system
+
 # 讀取檔案
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue # 繼續 跳到下一迴
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'): # 檢查是否有這個檔案
+    print('yeah! 找到檔案了！')
+    with open('products.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue # 繼續 跳到下一迴
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+
+else:
+    print('找不到檔案...')
 
 # 讓使用者輸入
 while True:
